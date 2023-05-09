057bd3cfebe8bbbfdc6a78066bbfb428cdd4d130
TheKarmatic <113950363+TheKarmatic@users.noreply.github.com>
2023-05-09 16:55:02 -0400

    Fixed countdown

--- Assets/Shiva Stuffs/Scripts/Countdown.cs
+++ Assets/Shiva Stuffs/Scripts/Countdown.cs
@@ -3,42 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 
 public class Countdown : MonoBehaviour
 {
     public string[] Messages; //3, 2, 1, Go!
-    public float interval = 1f;
+    public float countdownDuration;
     private TextMeshProUGUI countdownText = null;
-    public List<Rigidbody> rigidbodies = new List<Rigidbody>();
-    public GameObject machineGun;
+    private bool isCountdownRunning;
 
-    public IEnumerator Start()
+    private void Start()
     {
         countdownText = GetComponent<TextMeshProUGUI>();
-        rigidbodies.AddRange(FindObjectsOfType<Rigidbody>());
-        machineGun.GetComponent<MachineGun>().enabled = false;
+        StartCoroutine(CountdownCoroutine());
+        Time.timeScale = 0.0f;
+    }
 
-        foreach (Rigidbody rb in rigidbodies)
+    public IEnumerator CountdownCoroutine()
+    {
+        float startTime = Time.realtimeSinceStartup;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < countdownDuration)
         {
-            rb.isKinematic = true;
+            yield return null;
+            elapsedTime = Time.realtimeSinceStartup - startTime;
+
+            if (Messages.Length > Mathf.FloorToInt(elapsedTime))
+            {
+                countdownText.text = Messages[Mathf.FloorToInt(elapsedTime)];
+            }
+
+            Debug.Log("Remaining time: " + (countdownDuration - elapsedTime));
         }
 
-        int MessageDisplay = Messages.Length - 1;
-
-        while (MessageDisplay >= 0)
-        {
-            //print(Messages[MessageDisplay]);
-            countdownText.text = Messages[MessageDisplay];
-            yield return new WaitForSeconds(interval);
-            MessageDisplay -= 1;
-        }
-
-        foreach (Rigidbody rb in rigidbodies)
-        {
-            rb.isKinematic = false;
-        }
-
-        machineGun.GetComponent<MachineGun>().enabled = true;
+        Time.timeScale = 1.0f;
         gameObject.SetActive(false);
+    }
+
+    private void StartCountdown()
+    {
+        if (!isCountdownRunning)
+        {
+            StartCoroutine(CountdownCoroutine());
+            isCountdownRunning = true;
+        }
     }
 }
