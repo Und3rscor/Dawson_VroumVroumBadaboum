bdcaa764d934d4ce2ce66486894863edd1e5c6c4
TheKarmatic <113950363+TheKarmatic@users.noreply.github.com>
2023-05-13 20:15:27 -0400

    Settings menu setup

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,9 +5,42 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject mainMenu, settingsMenu;
+
+    private void Start()
+    {
+        RedrawMenu(mainMenu);
+    }
+
+    private void RedrawMenu(GameObject active)
+    {
+        if (mainMenu != null)
+        {
+            mainMenu.SetActive(false);
+        }
+        if (settingsMenu != null)
+        {
+            settingsMenu.SetActive(false);
+        }
+        if (active != null)
+        {
+            active.SetActive(true);
+        }
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Settings()
+    {
+        RedrawMenu(settingsMenu);
+    }
+
+    public void Back()
+    {
+        RedrawMenu(mainMenu);
     }
 
     public void QuitGame()
