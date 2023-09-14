0e25fc3d7eefc6e2834e098058c268ea502f15e4
TheKarmatic <113950363+TheKarmatic@users.noreply.github.com>
2023-09-14 12:56:28 -0400

    Forgot to merge main back in

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,6 @@
 using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
