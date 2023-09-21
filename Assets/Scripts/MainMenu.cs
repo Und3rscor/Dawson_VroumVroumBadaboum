5a5d3f32ae2840f73e89059424b601544ed76652
Und3rscor <47045695+Und3rscor@users.noreply.github.com>
2023-09-20 20:42:51 -0400

    class update

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -12,6 +12,11 @@ public class MainMenu : MonoBehaviour
     public GameObject masterAudio;
     private Slider volumeSlider;
 
+    // for play local menu
+    //public GameObject localPlayButton; // start button
+    //private int activePlayers = 0;
+    //private bool[] playerReadyStatus;
+
     private void Start()
     {
         RedrawMenu(menus[0]);
@@ -93,7 +98,39 @@ public class MainMenu : MonoBehaviour
 
     #endregion
 
+    #region Play-Local
+    /*
+    // je veut quoi ? je veut que quand un joueur press A son menu s'affiche et quand tout les joueurs actifs on click on ready le bouton play s'affiche
+    public void LocalPlayScreen()
+    {
+        playerReadyStatus = new bool[4];
+        for (int i = 0; i < playerReadyStatus.Length; i++)
+        {
+            playerReadyStatus[i] = false;
+        }
+    }
 
+    private void CheckAllPlayersReady()
+    {
+        bool allReady = true;
+        for (int i = 0; i < playerReadyStatus.Length; i++)
+        {
+            if (!playerReadyStatus[i])
+            {
+                allReady = false;
+                break;
+            }
+        }
+
+        // If all players are ready, show the "Start Game" button
+        if (allReady)
+        {
+            // Show the "Start Game" button or perform any other desired action
+        }
+    }
+
+    */
+    #endregion
 
     public void QuitGame()
     {
