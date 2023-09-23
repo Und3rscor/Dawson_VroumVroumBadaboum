514f47a58eef1eba4c55d5aa92a194d6adc25e37
Und3rscor <47045695+Und3rscor@users.noreply.github.com>
2023-09-23 19:10:51 -0400

    Play_Local Done

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -15,10 +15,7 @@ public class MainMenu : MonoBehaviour
     // for the CTA
     public GameObject canevas2;
 
-    // for play local menu
-    //public GameObject localPlayButton; // start button
-    //private int activePlayers = 0;
-    //private bool[] playerReadyStatus;
+        
 
     private void Start()
     {
@@ -111,39 +108,7 @@ public class MainMenu : MonoBehaviour
 
     #endregion
 
-    #region Play-Local
-    /*
-    // je veut quoi ? je veut que quand un joueur press A son menu s'affiche et quand tout les joueurs actifs on click on ready le bouton play s'affiche
-    public void LocalPlayScreen()
-    {
-        playerReadyStatus = new bool[4];
-        for (int i = 0; i < playerReadyStatus.Length; i++)
-        {
-            playerReadyStatus[i] = false;
-        }
-    }
-
-    private void CheckAllPlayersReady()
-    {
-        bool allReady = true;
-        for (int i = 0; i < playerReadyStatus.Length; i++)
-        {
-            if (!playerReadyStatus[i])
-            {
-                allReady = false;
-                break;
-            }
-        }
-
-        // If all players are ready, show the "Start Game" button
-        if (allReady)
-        {
-            // Show the "Start Game" button or perform any other desired action
-        }
-    }
-
-    */
-    #endregion
+  
 
     public void QuitGame()
     {
